c35bfa238c498ef645ca9cf5c43d8fd2ad07a72a
IlyaPrigogine <prigogine@protonmail.com>
2022-11-19 15:10:32 +0800

    Update YourContract.sol

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -2,14 +2,11 @@
 pragma solidity ^0.8.13;
 
 contract YourContract {
-    // coding convention to uppercase constant variables
-    address public immutable MY_ADDRESS;
-    uint public immutable MY_UINT;
-
-    constructor() {
-        MY_ADDRESS = msg.sender;
-        MY_UINT = 1;
+    uint public num;
+    function set(uint _num) public {
+        num = _num;
     }
-
-
+    function get() public view returns (uint) {
+        return num;
+    }
 }
