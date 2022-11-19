8ac4399a45ee17b3e827461a602237bb0c7c1bfa
IlyaPrigogine <prigogine@protonmail.com>
2022-11-19 16:24:45 +0800

    submitted

---
+++ packages/hardhat/contracts/TestContracts/MappingUINT.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract MappingUINT {
+    mapping (address => uint)  mapUint;
+
+    function setUint(address _addr, uint _value) public {
+        mapUint[_addr] = _value;
+    }
+    function getUint(address _addr) public view returns(uint) {
+        return mapUint[_addr];
+    }
+    function removeUint(address _addr) public {
+        delete mapUint[_addr];
+    }
+}
