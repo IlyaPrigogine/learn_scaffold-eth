d414ea4de0f2521d39ede71df5b623ae9a716e13
IlyaPrigogine <prigogine@protonmail.com>
2022-11-19 15:18:26 +0800

    Update YourContract.sol

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -2,11 +2,20 @@
 pragma solidity ^0.8.13;
 
 contract YourContract {
-    uint public num;
-    function set(uint _num) public {
-        num = _num;
+    uint public oneWei = 1 wei;
+    bool public isOneWei = oneWei == 1;
+
+    uint public oneEther = 1 ether;
+    bool public isOneEther = oneEther == 1e18;
+
+    function setOneWei(uint _n) public {
+        oneWei = _n;
     }
-    function get() public view returns (uint) {
-        return num;
+    function setOneEther(uint _n) public {
+        oneEther = _n * 1e18;
+    }
+
+    function getIsOneEther() public view returns(bool) {
+        return oneEther == 1e18 ? true: false;
     }
 }
