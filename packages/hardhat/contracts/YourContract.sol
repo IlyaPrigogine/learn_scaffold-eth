df48bbac0a78848dcabdcfb61df02369c9c4f88a
IlyaPrigogine <prigogine@protonmail.com>
2022-11-19 16:34:34 +0800

    Update YourContract.sol

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -2,15 +2,14 @@
 pragma solidity ^0.8.13;
 
 contract YourContract {
-    mapping (address => uint)  mapUint;
-
-    function setUint(address _addr, uint _value) public {
-        mapUint[_addr] = _value;
+    mapping (address => bool) MapBool;
+    function get(address _addr) public view returns(bool) {
+        return MapBool[_addr];
     }
-    function getUint(address _addr) public view returns(uint) {
-        return mapUint[_addr];
+    function set(address _addr, bool _value) public {
+        MapBool[_addr] = _value;
     }
-    function removeUint(address _addr) public {
-        delete mapUint[_addr];
+    function remove(address _addr) public {
+        delete MapBool[_addr];
     }
 }
