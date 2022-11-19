83c22bdfa4dc14361968d3f32e3f46ab3597da76
IlyaPrigogine <prigogine@protonmail.com>
2022-11-19 14:37:57 +0800

    submitted

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -1,33 +1,7 @@
-pragma solidity >=0.8.0 <0.9.0;
-//SPDX-License-Identifier: MIT
-
-import "hardhat/console.sol";
-// import "@openzeppelin/contracts/access/Ownable.sol";
-// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
+// SPDX-License-Identifier: MIT
+// compiler version must be greater than or equal to 0.8.13 and less than 0.9.0
+pragma solidity ^0.8.13;
 
 contract YourContract {
-
-  event SetPurpose(address sender, string purpose);
-
-  string public purpose = "Building Unstoppable Apps!!!";
-    uint public ratio;
-
-  constructor() payable {
-    // what should we do on deploy?
-  }
-
-  function setPurpose(string memory newPurpose) public {
-      purpose = newPurpose;
-      console.log(msg.sender,"set purpose to",purpose);
-      emit SetPurpose(msg.sender, purpose);
-  }
-
-    function setRatio(uint  _ratio) public {
-        ratio = _ratio;
-        console.log(msg.sender,"set ratio to",ratio);
-    }
-
-  // to support receiving ETH by default
-  receive() external payable {}
-  fallback() external payable {}
+    string public greet = "Hello World!";
 }
