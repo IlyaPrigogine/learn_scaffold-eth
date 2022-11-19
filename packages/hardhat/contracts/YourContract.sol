4a5ecb06e0a0162c478b42d8843e011cb0aadfd8
IlyaPrigogine <prigogine@protonmail.com>
2022-11-19 15:51:08 +0800

    Update YourContract.sol

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -2,26 +2,40 @@
 pragma solidity ^0.8.13;
 
 contract YourContract {
-    uint public result_i;
-    uint public result_j;
-    function loop() public {
-        uint i;
-        uint j;
-        for ( i = 0; i < 10; i++) {
-            if (i == 3) {
-                // Skip to next iteration with continue
-                continue;
-            }
-            if (i == 5) {
-                // Exit loop with break
-                break;
-            }
-        }
+    mapping(address => uint) public myMap;
 
-        while (j < 10) {
-            j++;
-        }
-        result_i = i;
-        result_j = j;
+    function get(address _addr) public view returns (uint) {
+        return myMap[_addr];
+    }
+
+    function set(address _addr, uint _i) public {
+        myMap[_addr] = _i;
+    }
+
+    function remove(address _addr) public {
+        delete myMap[_addr];
+    }
+}
+
+contract NestedMapping {
+    // Nested mapping (mapping from address to another mapping)
+    mapping(address => mapping(uint => bool)) public nested;
+
+    function get(address _addr1, uint _i) public view returns (bool) {
+        // You can get values from a nested mapping
+        // even when it is not initialized
+        return nested[_addr1][_i];
+    }
+
+    function set(
+        address _addr1,
+        uint _i,
+        bool _boo
+    ) public {
+        nested[_addr1][_i] = _boo;
+    }
+
+    function remove(address _addr1, uint _i) public {
+        delete nested[_addr1][_i];
     }
 }
