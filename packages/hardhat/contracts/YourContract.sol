42e416e052ba59f1ce51f5433297fbfc1925c9a2
IlyaPrigogine <prigogine@protonmail.com>
2022-11-28 18:04:25 +0800

    Update YourContract.sol

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -1,24 +1,36 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >0.4.23 <0.9.0;
-import "./Foundation.sol";
+pragma solidity ^0.8.13;
+
 contract YourContract {
-    Foundation[] private _foundations;
+    uint[] public arr;
+    uint[] public arr2 = [1, 2, 3];
+    uint[10] public myFixedSizeArr;
 
-    function createFoundation(
-        string memory name
-    ) public {
-        Foundation foundation = new Foundation(
-            name,
-            msg.sender
-        );
-        _foundations.push(foundation);
-    }
-
-    function allFoundations(uint256 limit, uint256 offset)
-    public
-    view
-    returns (Foundation[] memory coll)
-    {
-        return coll;
-    }
+//    function get(uint i) public view returns (uint) {
+//        return arr[i];
+//    }
+//
+//    function getArr() public view returns (uint[] memory) {
+//        return arr;
+//    }
+//
+//    function push(uint i) public {
+//        arr.push(i);
+//    }
+//
+//    function pop() public {
+//        arr.pop();
+//    }
+//
+//    function getLength() public view returns (uint) {
+//        return arr.length;
+//    }
+//
+//    function remove(uint index) public {
+//        delete arr[index];
+//    }
+//
+//    function examples() external {
+//        uint[] memory a = new uint[](5);
+//    }
 }
