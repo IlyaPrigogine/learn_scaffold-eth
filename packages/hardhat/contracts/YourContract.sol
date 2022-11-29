35c62e5fb67cdbf03128c1de0799dcff91a872a0
IlyaPrigogine <prigogine@protonmail.com>
2022-11-29 13:20:01 +0800

    Update YourContract.sol

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -2,31 +2,5 @@
 pragma solidity ^0.8.13;
 
 contract YourContract {
-    enum Status {
-        Pending,
-        Accepted,
-        Rejected,
-        Canceled
-    }
-    Status status;
-
-    function get() public view returns(Status) {
-        return status;
-    }
-
-    function cancel() public {
-        status = Status.Canceled;
-    }
-
-    function accept() public {
-        status = Status.Accepted;
-    }
-
-    function reject() public {
-        status = Status.Accepted;
-    }
-
-    function reset() public {
-        delete status;
-    }
+    
 }
