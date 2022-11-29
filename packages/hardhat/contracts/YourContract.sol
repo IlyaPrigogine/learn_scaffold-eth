5d7d582c6b2fb129f1cdc3f60df44e5645f58e7a
IlyaPrigogine <prigogine@protonmail.com>
2022-11-29 12:56:09 +0800

    Update YourContract.sol

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -4,28 +4,7 @@ pragma solidity ^0.8.13;
 contract YourContract {
     uint[] arr2 = [1, 2, 3];
 
-
-    function get(uint _i) public view returns(uint) {
-        return arr2[_i];
-    }
-
-    function getArr() public view returns(uint[] memory) {
-        return arr2;
-    }
-
-    function push(uint _i) public {
-        arr2.push(_i);
-    }
-
-    function pop() public {
-        arr2.pop();
-    }
-
-    function getLength() public view returns(uint) {
-        return arr2.length;
-    }
-
-    function remove(uint _index) public {
-        delete arr2[_index];
+    function getValue(uint _index) public returns(uint) {
+        return arr2[_index];
     }
 }
