75f629841d52d4c171916524c8316d5fd4baeddf
IlyaPrigogine <prigogine@protonmail.com>
2022-11-30 11:56:50 +0800

    Update YourContract.sol

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -3,20 +3,20 @@ pragma solidity ^0.8.13;
 
 contract YourContract {
     uint public balance;
-    uint public constant MAX_UINT = 2**256 - 1;
-
     function deposit(uint _amount) public {
         uint oldBalance = balance;
-        uint newBalance = balance + _amount;
+        uint newBalance = oldBalance + _amount;
         require(newBalance >= oldBalance, "OverFlow");
-        balance = newBalance;
-        assert(balance >= oldBalance);
+        balance += _amount;
+
+        assert(balance>=oldBalance);
     }
 
     function withdraw(uint _amount) public {
         uint oldBalance = balance;
-        require(balance >= _amount, "underflow");
+        require(_amount <= balance, "no enough");
         balance -= _amount;
-        assert (balance <= oldBalance);
+
+        assert(balance<=oldBalance);
     }
 }
