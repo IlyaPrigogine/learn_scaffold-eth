66a55cf8b56cbe82a847690b69c7f3266245ac98
IlyaPrigogine <prigogine@protonmail.com>
2022-11-30 12:29:29 +0800

    Update YourContract.sol

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -2,23 +2,22 @@
 pragma solidity ^0.8.13;
 
 contract YourContract {
-    function returnMany() public pure returns (uint, bool, uint) {
-        return (1, true, 2);
+    function returnMany() public pure returns (uint x, bool b, uint y) {
+        x = 1; b = true; y = 2;
     }
 
-    function named() public pure returns (uint x, bool b, uint y) {
-        x = 3; b = false; y = 4;
+    uint public balance = 2;
+    function returnMany2() public view returns(uint x, bool b, uint y){
+        x = balance - 1;
+        y = balance + 1;
+        b = false;
     }
 
-    function destructuringAssignments() public pure returns (uint, bool, uint, uint, uint) {
-        (uint i, bool b, uint j) = returnMany();
-        (uint x, , uint y) = (4, 5, 6);
-        return (i, b, j, x, y);
+    function inc() public {
+        balance +=1;
     }
 
-    function destructuringAssignments2() public pure returns (uint i, bool b, uint j, uint x, uint y) {
-        ( i,  b,  j) = returnMany();
-        ( x, ,  y) = (4, 5, 6);
-        return (i, b, j, x, y);
+    function dec() public {
+        balance -=1;
     }
 }
