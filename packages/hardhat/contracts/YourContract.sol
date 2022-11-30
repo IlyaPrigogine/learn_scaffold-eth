4be716d7c19d3e4b47f91b00830fb32f7e6745cd
IlyaPrigogine <prigogine@protonmail.com>
2022-11-30 12:55:21 +0800

    Update YourContract.sol

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -1,23 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-contract X {
-    string public name;
-    constructor (string memory _name) {
-        name = _name;
-    }
-}
-
-contract Y {
-    uint public y;
+contract YourContract {
+    address public owner;
+    uint public balance;
     constructor () {
+        owner = msg.sender;
+    }
+    function setOwner() public {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "not owner");
+        _;
+    }
+
+    function setBalance(uint _balance) public onlyOwner {
+        balance = _balance;
     }
 }
-
-contract YourContract is X,Y{
-    constructor () X("X was called") {
-
-    }
-}
-
 
