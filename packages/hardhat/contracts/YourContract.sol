2fe60aebd93523d5a82a273db2c4f059e4aa0dc0
IlyaPrigogine <prigogine@protonmail.com>
2022-11-30 13:50:16 +0800

    Update YourContract.sol

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -1,8 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+contract A {
+    function foo() public view virtual returns(string memory) {
+        return "A";
+    }
+}
 
-contract YourContract  {
+contract B is A{
+    function foo() public view override returns(string memory) {
+        return "B";
+    }
+}
+
+contract YourContract is A {
 
 }
 
