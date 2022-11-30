7f5c6052ed49f2dccae8ed6cb0b1c3d7b5919e4c
IlyaPrigogine <prigogine@protonmail.com>
2022-11-30 13:28:30 +0800

    Update YourContract.sol

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -1,27 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-contract X {
-    string public XName;
-    constructor (string memory _name) {
-        XName = _name;
-    }
-}
-
-contract Y {
-    string public YName;
-    constructor (string memory _name) {
-        YName = _name;
-    }
-}
-contract YourContract is X,Y{
-    address public owner;
-    constructor () X("X world") Y("Y world"){
-        owner = msg.sender;
+contract YourContract {
+    struct Todo {
+        string text;
+        bool completed;
+        uint timestamp;
     }
 
-    function setName(string memory _XName, string memory _YName) public {
-        XName = _XName;
-        YName = _YName;
+    Todo[] public todos;
+    /* public functions*/
+    function create(string memory _text) public {
+        Todo memory todo;
+        todo.text = _text;
+        todo.timestamp = block.timestamp;
+        todos.push(todo);
+    }
+
+    function toggleCompleted(uint _index) public {
+        todos[_index].completed = !todos[_index].completed;
+        todos[_index].timestamp = block.timestamp;
+    }
+
+    /* views */
+    function get(uint _index) public view returns(Todo memory) {
+        return todos[_index];
+    }
+
+    function getLength() public view returns(uint) {
+        return todos.length;
     }
 }
