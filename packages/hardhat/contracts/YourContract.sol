c623363fd34712c881a4b82376c6cb09217758de
IlyaPrigogine <prigogine@protonmail.com>
2022-11-30 12:16:05 +0800

    Update YourContract.sol

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.13;
 
 contract YourContract {
-    event Log(address indexed sender,string message);
+    event Log(address indexed sender, string message);
 
     function test() public {
-        emit Log(msg.sender, "TEST LOG");
+        emit Log(msg.sender, "test log");
     }
 }
