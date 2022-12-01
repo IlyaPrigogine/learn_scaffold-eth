9ac54ad5aea2d4623ad109b2fe0dd50e1424620a
IlyaPrigogine <prigogine@protonmail.com>
2022-12-01 13:53:22 +0800

    Update YourContract.sol

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -1,25 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+contract A {
 
-contract MyToken is ERC20 {
-    constructor() ERC20("MyToken", "MTK") {}
 }
 
-contract YourContract {
-    function aaa(address _addr) public view returns (string memory) {
-        return IERC20Metadata(_addr).name();
-    }
-
-    function bbb(address _addr) public view returns (string memory) {
-        return IERC20Metadata(_addr).symbol();
-    }
-
-    function ccc(address _addr) public view returns (uint) {
-        return IERC20Metadata(_addr).decimals();
-    }
+contract YourContract is A {
+    
 }
 
 
