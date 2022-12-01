67285ce0f8f9f765a2a3f5f888e8e602507856e0
IlyaPrigogine <prigogine@protonmail.com>
2022-12-01 17:51:50 +0800

    Update YourContract.sol

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -1,55 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-interface IStore {
-    function name() external view returns(string memory);
-    function loc() external view returns(string memory);
-}
-
-interface IBookStore is IStore {
-    function PV() external view returns(uint);
-}
-
-contract BookStore {
-    string public name;
-    string public loc;
-    uint public PV;
-    constructor (string memory _name, string memory _loc, uint _PV) {
-        name = _name;
-        loc = _loc;
-        PV = _PV;
+contract Foo{
+    address public owner;
+    constructor (address _addr) {
+        owner = _addr;
     }
 }
 
-contract Store {
-    string public name;
-    string public loc;
-    constructor (string memory _name, string memory _loc) {
-        name = _name;
-        loc = _loc;
-    }
+interface IFoo {
+    function owner() external view returns(address);
 }
 
-contract YourContract {
-    address public curStoreAddress;
-    function create(string memory _name, string memory _loc, uint _PV) public{
-        BookStore bs = new BookStore(_name, _loc, _PV);
-        curStoreAddress = address (bs);
+contract YourContract{
+    address public curAddr;
+    function create() public {
+        Foo foo = new Foo(msg.sender);
+        curAddr = address (foo);
     }
 
-    function create(string memory _name, string memory _loc) public {
-        Store s = new Store(_name, _loc);
-        curStoreAddress = address (s);
-    }
-
-    function getBookStore() public view returns(string memory name, string memory loc,uint PV) {
-        name = IBookStore(curStoreAddress).name();
-        loc  = IBookStore(curStoreAddress).loc();
-        PV = IBookStore(curStoreAddress).PV();
-    }
-
-    function getStore() public view returns(string memory name, string memory loc) {
-        name = IStore(curStoreAddress).name();
-        loc  = IStore(curStoreAddress).loc();
+    function getOwner() public view returns(address) {
+        return IFoo(curAddr).owner();
     }
 }
