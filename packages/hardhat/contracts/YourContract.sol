827a78cc81add1e3fb4318c65a7fbd757573e0cf
IlyaPrigogine <prigogine@protonmail.com>
2022-12-01 14:12:16 +0800

    Update YourContract.sol

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-contract A {
+contract YourContract  {
+    receive() external payable {}
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
 
-}
-
-contract YourContract is A {
-    
+    uint public num;
 }
 
 
