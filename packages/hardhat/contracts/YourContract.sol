6b1bb276e518ebc7f5bdaa0ff71dbc529e010aca
IlyaPrigogine <prigogine@protonmail.com>
2022-12-01 11:34:08 +0800

    Update YourContract.sol

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -5,19 +5,20 @@ contract A {
     string public message;
     uint public num;
     function _foo() internal {
-        message = string.concat(message,"A");
+        message = string.concat(message, "A");
     }
 
     function _bar() internal {
         num += 1;
     }
 }
-contract YourContract is A {
-    function bar() public {
-        super._bar();
-    }
 
+contract YourContract is A{
     function foo() public {
         super._foo();
+    }
+
+    function bar() public {
+        super._bar();
     }
 }
