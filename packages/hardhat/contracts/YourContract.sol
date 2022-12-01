2e5ee9f348c9dbcb27d25ed8cffdcd631c5ec113
IlyaPrigogine <prigogine@protonmail.com>
2022-12-01 17:26:21 +0800

    submitted

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -1,40 +1,55 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-interface IBookStore {
+interface IStore {
     function name() external view returns(string memory);
-    function location() external view returns(string memory);
+    function loc() external view returns(string memory);
+}
+
+interface IBookStore is IStore {
     function PV() external view returns(uint);
 }
+
 contract BookStore {
     string public name;
-    string public location;
+    string public loc;
     uint public PV;
-
-    constructor(string memory _name, string memory _location, uint _PV) {
+    constructor (string memory _name, string memory _loc, uint _PV) {
         name = _name;
-        location = _location;
+        loc = _loc;
         PV = _PV;
     }
 }
 
-contract YourContract {
-    BookStore[] public bookStores;
-
-    /* public functions */
-    function create(string memory _name, string memory _loc, uint _PV) public {
-        BookStore bs = new BookStore(_name,_loc, _PV);
-        bookStores.push(bs);
-    }
-
-    /* views */
-    function getBookStores() public view returns(BookStore[] memory) {
-        return bookStores;
-    }
-
-    function getBookStore(uint _index) public view returns(string memory _name, string memory _loc, uint _PV) {
-        BookStore bs = bookStores[_index];
-        return ( bs.name(), bs.location(), bs.PV());
+contract Store {
+    string public name;
+    string public loc;
+    constructor (string memory _name, string memory _loc) {
+        name = _name;
+        loc = _loc;
     }
 }
 
+contract YourContract {
+    address public curStoreAddress;
+    function create(string memory _name, string memory _loc, uint _PV) public{
+        BookStore bs = new BookStore(_name, _loc, _PV);
+        curStoreAddress = address (bs);
+    }
+
+    function create(string memory _name, string memory _loc) public {
+        Store s = new Store(_name, _loc);
+        curStoreAddress = address (s);
+    }
+
+    function getBookStore() public view returns(string memory name, string memory loc,uint PV) {
+        name = IBookStore(curStoreAddress).name();
+        loc  = IBookStore(curStoreAddress).loc();
+        PV = IBookStore(curStoreAddress).PV();
+    }
+
+    function getStore() public view returns(string memory name, string memory loc) {
+        name = IStore(curStoreAddress).name();
+        loc  = IStore(curStoreAddress).loc();
+    }
+}
