fbc88b6adc433e5141278c84a2f9dc1ce213c125
IlyaPrigogine <prigogine@protonmail.com>
2022-12-03 13:15:14 +0800

    Update YourContract.sol

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -1,16 +1,36 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract MTK is ERC20 {
-    constructor() ERC20("MTK", "MTK") {
-        super._mint(msg.sender, 10000e18);
+contract Car {
+    string public name;
+    uint public price;
+    constructor (string memory _name, uint _price) {
+        name = _name;
+        price = _price;
     }
 }
 
-
-contract YourContract  {
-    function foo(address _addr) public view returns(string memory) {
-        return IERC20Metadata(_addr).name();
+contract YourContract {
+    address[] carAddresses;
+    function create(string memory name, uint price) public {
+        Car car = new Car(name, price);
+        carAddresses.push(address(car));
+    }
+    /* public views*/
+    function getCar(uint _index) public view returns (string memory name, uint, address){
+        address carAddress = carAddresses[_index];
+        Car car = Car(carAddress);
+        return (
+        car.name(),
+        car.price(),
+        carAddress
+        );
+    }
+    function getCarLength() public view returns(uint) {
+        return carAddresses.length;
+    }
+    function getCarFromAddress(address _addr) public view returns(string memory name, uint) {
+        Car car = Car(_addr);
+        return (car.name(), car.price());
     }
 }
