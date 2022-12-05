9174f668e52d28b2b9e33359ff7580cd96065a91
IlyaPrigogine <prigogine@protonmail.com>
2022-12-05 13:29:11 +0800

    submitted

--- packages/hardhat/contracts/TestContracts3/NFT.sol
+++ packages/hardhat/contracts/TestContracts3/NFT.sol
@@ -2,11 +2,21 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+
+contract ERC721Holder is IERC721Receiver {
+    function onERC721Received(
+        address,
+        address,
+        uint256,
+        bytes memory
+    ) public virtual override returns (bytes4) {
+        return this.onERC721Received.selector;
+    }
+}
 
 contract YC is ERC721 {
     constructor() ERC721("YC", "YC") {}
-
     uint public n;
     function mint() public {
         n++;
@@ -18,15 +28,11 @@ interface IYC {
     function mint() external;
 }
 
-contract YourContract is ERC721Holder {
+contract YCR is ERC721Holder {
     address public tokenAddress;
-    constructor() {
+    constructor(address _addr) {
+        tokenAddress = _addr;
     }
-
-    function setToken(address _address) public {
-        tokenAddress = _address;
-    }
-
     function mint() public {
         IYC(tokenAddress).mint();
     }
