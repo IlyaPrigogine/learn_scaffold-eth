f7ee08e91d13da92dd2ea22582cd249c0c7938c4
IlyaPrigogine <prigogine@protonmail.com>
2022-12-05 17:14:47 +0800

    Update YourContract.sol

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -1,22 +1,50 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract YC is ERC721 {
-    constructor() ERC721("YC", "YC") {}
-
-    uint public n;
-    function mint() public {
-        n++;
-        _safeMint(msg.sender,n);
+contract TokenA is ERC20 {
+    constructor() ERC20("TokenA","TKA") {
+        super._mint(msg.sender,10000e18);
     }
 }
 
-interface IYC {
-    function mint() external;
+contract TokenB is ERC20 {
+    constructor() ERC20("TokenB","TKB") {
+        super._mint(msg.sender,10000e18);
+    }
+}
+
+contract Staking {
+    ERC20 token;
+    constructor (address _addr) {
+        token = ERC20(_addr);
+    }
+
+    function stake(address _from, uint _amount) external {
+        token.transferFrom(_from, address(this), _amount);
+    }
+}
+
+interface IStaking{
+    function stake(address _from, uint _amount) external;
 }
 
 contract YourContract {
+    mapping(address => address) public stakings;
+
+    function createStaking(address _erc20Address) public {
+        Staking s = new Staking(_erc20Address);
+        stakings[_erc20Address] = address(s);
+    }
+
+    function stake(address _erc20Address, uint _amount) public {
+        require(stakings[_erc20Address] != address(0),"no stakings");
+        address staking = stakings[_erc20Address];
+        IStaking(staking).stake(msg.sender, _amount);
+    }
+
+    function getStaking(address _erc20Address) public view returns(address) {
+        return stakings[_erc20Address];
+    }
 }
